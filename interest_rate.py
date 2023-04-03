e707b283d1bedf4652158d8efdc99cdd879e09fb
finzellt <chimera31@gmail.com>
2023-04-03 14:52:08 -0500

    Modified python scripts to include problem statements.

--- interest_rate.py
+++ interest_rate.py
@@ -1,5 +1,13 @@
 #calculating interest rate
 
+'''
+1. Partner up with one of your neighbors. You only need to have one person coding, but both of you should be able to see the monitor.
+2. Calculate by hand the amount of money you’ll have in your account after five years.
+3. Download a piece of code which can be found here: LINK TO CODE. Run the code and compare it to your by-hand results.
+4. Debug the provided code.
+5. Modify the code so that you keep track of all of the values of funds at the different steps.
+'''
+
 t_total = 5 #total time in years
 interest_rate = 0.05 #annual interest rate
 funds_t0 = 1500 #total funds at time 0
--- list_practice.py
+++ list_practice.py
@@ -1,3 +1,10 @@
 #calculating interest rate
 
+'''
+1. Define a list called letters with the following values: “A”, “B”, and “C” (in that order)
+2. Write a line of code with a print statement that uses the letters list to print out the word “CAB” (I.e., “C”, “A”, and “B”).
+3. Modify your code from above to append the values “D” and “E” to your list. Change the print statement so that it prints out word “DECADE”.
+'''
+
+
 my_list = ["A","B","C"]
